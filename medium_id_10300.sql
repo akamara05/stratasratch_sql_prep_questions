c3896cf930a70296793a1acbd4633c12c754d0b6
Aisha Kamara <73575069+akamara05@users.noreply.github.com>
2026-08-18 22:26:04 -0400

    Add files via upload

--- medium_id_10300.sql
+++ medium_id_10300.sql
@@ -1,12 +1,6 @@
 /*
-Question: 
-Find the total number of downloads for paying and non-paying users by date. 
-Include only records where non-paying customers have more downloads than paying customers. 
-The output should be sorted by earliest date first and contain 3 columns date, non-paying downloads, paying downloads.
-
-My Response: 
-Each table seems to hold just a few columns of required information, so my first 
-step is to join the tables and create a CTE 'Base' table to then query from. 
+Each table seems to hold just a few columns of required information
+so my first step is to join the tables and create a CTE 'Base' table to then query from. 
 */
 WITH base AS (
     SELECT
@@ -18,7 +12,7 @@ WITH base AS (
     LEFT JOIN ms_user_dimension AS udim USING(acc_id)
     LEFT JOIN ms_download_facts AS facts USING (user_id)
 ),
--- Sum the number of downloads for paying vs non-paying customers.
+-- Sum the number of downloads for paying vs non-paying
 downloads AS (
     SELECT
         date,
@@ -35,4 +29,5 @@ FROM downloads
 WHERE
     non_paying_downloads > paying_downloads
 -- Per instructions: The output should be sorted by earliest date first.
-ORDER BY date
+ORDER BY
+    date
