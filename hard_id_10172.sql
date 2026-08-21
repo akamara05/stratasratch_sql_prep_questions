38280d8917acd059e8a80c27f51dee277d4d42cf
Aisha Kamara <73575069+akamara05@users.noreply.github.com>
2026-08-21 17:51:27 -0400

    Add additional comments

--- hard_id_10172.sql
+++ hard_id_10172.sql
@@ -1,23 +1,34 @@
+/*
+Question: Find the best-selling item for each month (no need to separate months by year). 
+The best-selling item is determined by the highest total sales amount, calculated as: total_paid = unitprice * quantity. 
+A negative quantity indicates a return or cancellation (the invoice number begins with 'C'. To calculate sales, 
+ignore returns and cancellations. Output the month, description of the item, and the total amount paid
+*/
+
 WITH totals_paid AS (
+-- Per instructions, no need to separate months by year.
 SELECT month(invoicedate) AS month,
 description,
 SUM(unitprice*quantity) AS total_paid
 FROM online_retail
--- excludes cancelled orders
+-- Excludes canceled orders
 WHERE invoiceno NOT LIKE 'C%'
 GROUP BY 1, 2
--- excludes any returns marked by a negative vlaue 
+-- Excludes any returns marked by a negative value 
 HAVING total_paid >=0
 ), 
 ranking AS (
-SELECT month, 
+SELECT 
+month, 
 description, 
-total_paid, rank() over (PARTITION BY month ORDER BY total_paid DESC) as rankings
+total_paid, 
+rank() over (PARTITION BY month ORDER BY total_paid DESC) as rankings
 FROM totals_paid
 WHERE total_paid >= 0
 ORDER BY 1
 ) 
-SELECT month, 
+SELECT 
+month, 
 description, 
 total_paid
 FROM ranking
