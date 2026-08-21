5c366948268456e9dee85804f814a2132f9e7958
Aisha Kamara <73575069+akamara05@users.noreply.github.com>
2026-08-21 14:56:37 -0400

    Update medium_id_9915.sql

--- medium_id_9915.sql
+++ medium_id_9915.sql
@@ -1,5 +1,8 @@
 /* 
-Question: Find the customers with the highest daily total order cost between ** 2019-02-01 and 2019-05-01**. If a customer had more than one order on a certain day, sum the order costs on a daily basis. Output each customer's first name, total cost of their items, and the date. If multiple customers tie for the highest daily total on the same date, return all of them.
+Question: Find the customers with the highest daily total order cost between ** 2019-02-01 and 2019-05-01**. 
+If a customer had more than one order on a certain day, sum the order costs on a daily basis. Output each 
+customer's first name, total cost of their items, and the date. If multiple customers tie for the highest daily 
+total on the same date, return all of them.
 */
 
 -- Let's take a daily summary of said customers within the instructed timeline 2019-02-01 and 2019-05-01. 
@@ -17,7 +20,8 @@ ORDER BY 2 -- not necessary to order at this point, but easy for the analyst's r
 rankings AS (
 SELECT  
 t.*, 
--- Used dense rank window function based on these instructions: *If multiple customers tie for the highest daily total on the same date, return all of them.*
+-- Used dense rank window function based on these instructions: 
+-- *If multiple customers tie for the highest daily total on the same date, return all of them.*
 DENSE_RANK() OVER (PARTITION BY order_date ORDER BY total_daily_cost DESC) AS ranking,
 c.first_name
 FROM customer_totals as t 
