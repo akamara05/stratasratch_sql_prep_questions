3ebad16a2e15ab9ed7298e808f125b39a8d47a65
Aisha Kamara <73575069+akamara05@users.noreply.github.com>
2026-08-23 19:17:33 -0400

    Add question block.

--- hard_id_10547.sql
+++ hard_id_10547.sql
@@ -1,5 +1,15 @@
-WITH
-release_order AS (
+/* 
+Question: 
+You are given a dataset of actors and the films they have been involved in, including each film's release date and rating. 
+For each actor, calculate the difference between the rating of their most recent film and their average rating across all 
+previous films (the average rating excludes the most recent one).
+
+Return a list of actors along with their average rating of previous films, the rating of their most recent film, and the 
+difference between the two ratings. Round the difference calculation to 2 decimal places. If an actor has only one film, 
+return 0 for the difference and their only film’s rating for both the average and latest rating fields.
+*/
+
+WITH release_order AS (
 SELECT
 actor_name, 
 film_title,
@@ -11,10 +21,11 @@ FROM actor_rating_shift
 ), 
 averages AS (
 SELECT actor_name,
--- If an actor has only one film, return 0 for the difference and their only film’s rating for both the average and latest rating fields.
+-- If an actor has only one film, return 0 for the difference and their only film’s rating for both the average 
+-- and latest rating fields.
 CASE WHEN COUNT(film_title) = 1 THEN film_rating
 ELSE
--- the average rating excludes the most recent one
+-- The average rating excludes the most recent one
 AVG(CASE WHEN release_sequence > 1 THEN film_rating END) 
 END AS avg_rating,
 -- Most recent film rating
