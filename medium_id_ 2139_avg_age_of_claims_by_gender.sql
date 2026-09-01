2c32c5123bf574cc7cdbabac0b79320412ed101a
Aisha Kamara <73575069+akamara05@users.noreply.github.com>
2026-08-31 23:32:05 -0400

    Add title and instructions

--- medium_id_ 2139_avg_age_of_claims_by_gender.sql
+++ medium_id_ 2139_avg_age_of_claims_by_gender.sql
@@ -1,9 +1,16 @@
+/*
+Title: Average Age of Claims by Gender
+
+Instructions: You have been asked to calculate the average age by gender of people who filed more than 1 claim in 2021.
+The output should include the gender and average age rounded to the nearest whole number.
+*/
+
 WITH Base AS (
 SELECT 
 C.account_id,
 A.age,
 A.gender, 
-COUNT(DISTINCT claim_id) AS claim_count
+COUNT(DISTINCT C.claim_id) AS claim_count
 FROM cvs_claims AS C
 LEFT JOIN cvs_accounts AS A
 USING (account_id) 
@@ -11,7 +18,9 @@ WHERE YEAR(date_submitted) = '2021'
 GROUP BY 1,2,3
 ) 
 SELECT gender, 
+-- Average age rounded to the nearest whole number
 ROUND(AVG(age)) AS avg_age
 FROM Base
+-- Filed more than 1 claim
 WHERE claim_count > 1
 GROUP BY 1;
