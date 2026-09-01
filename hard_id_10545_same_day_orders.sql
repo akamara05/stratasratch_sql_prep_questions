7a712b885fb615be4734026df3a5c47fc80a4d59
Aisha Kamara <73575069+akamara05@users.noreply.github.com>
2026-08-31 22:59:09 -0400

    Add title and instructions

--- hard_id_10545_same_day_orders.sql
+++ hard_id_10545_same_day_orders.sql
@@ -1,3 +1,12 @@
+/* 
+Title: Same-Day Orders
+
+Instructions: 
+Identify users who started a session and placed an order on the same day.
+For these users, return the total number of orders placed on that day and the total order value for that day.
+Your output should include the user_id, the session_date, the total number of orders, and the total order value for that day.
+*/
+
 WITH Base AS 
 (SELECT
 DISTINCT S.session_date, 
