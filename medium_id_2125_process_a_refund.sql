252d861b8f0904387a236c5da6daaf8435a3c9b3
Aisha Kamara <73575069+akamara05@users.noreply.github.com>
2026-08-31 23:57:48 -0400

    Add title and instructions

--- medium_id_2125_process_a_refund.sql
+++ medium_id_2125_process_a_refund.sql
@@ -1,3 +1,11 @@
+/*
+Title: Process a Refund
+
+Instructions: Calculate and display the minimum, average and the maximum number of days it takes to process a refund for 
+accounts opened from January 1, 2019. Group by billing cycle in months.
+Note: The time frame for a refund to be fully processed is from settled_at until refunded_at.
+*/
+
 -- Join tables.
 WITH Base AS (
 SELECT
