735dd5da96fc47fc508ebbd7d907117e63f8e078
Aisha Kamara <73575069+akamara05@users.noreply.github.com>
2026-08-31 21:59:57 -0400

    Add title and instructions

--- medium_id_10318_new_products.sql
+++ medium_id_10318_new_products.sql
@@ -1,3 +1,11 @@
+/* 
+Title: New Products 
+
+Instructions: Calculate the net change in the number of products launched by companies in 2020 compared to 2019. 
+Your output should include the company names and the net difference.
+(Net difference = Number of products launched in 2020 - The number launched in 2019.)
+*/
+
 WITH TotalProducts AS (
 SELECT company_name, 
 COUNT(DISTINCT CASE WHEN year = 2019 THEN product_name END) AS products_2019,
@@ -8,5 +16,5 @@ GROUP BY 1
 SELECT 
 company_name, 
 (products_2020 - products_2019) AS total_launch 
-from TotalProducts
+FROM TotalProducts
 ORDER BY 1; 
