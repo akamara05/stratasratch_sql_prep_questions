9a001fe5efbd3daee9344d4c891878aba853b6be
Aisha Kamara <73575069+akamara05@users.noreply.github.com>
2026-09-11 00:14:46 -0400

    Add title and instructions

--- hard_id_ 2036_lowest_revenue_generated_restaurants.sql
+++ hard_id_ 2036_lowest_revenue_generated_restaurants.sql
@@ -1,3 +1,12 @@
+/* 
+Title: Lowest Revenue Generated Restaurants
+
+Instructions: Write a query that returns a list of the bottom 2% revenue generating restaurants. Return a list of restaurant IDs and their total revenue from when customers placed orders in May 2020.
+
+You can calculate the total revenue by summing the order_total column. And you should calculate the bottom 2% by partitioning 
+the total revenue into evenly distributed buckets.
+*/
+
 WITH cte AS 
 (
 SELECT 
