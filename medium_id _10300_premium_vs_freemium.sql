490bd519ac9440707a65caa72ec3f1d098d0bf8d
Aisha Kamara <73575069+akamara05@users.noreply.github.com>
2026-09-24 23:36:26 -0400

    Add files via upload

---
+++ medium_id _10300_premium_vs_freemium.sql
@@ -0,0 +1,43 @@
+WITH download_totals AS (
+    SELECT c.date AS download_date,
+    -- Based on a preview of the ms_download_facts table it makes more sense to use SUM instead of COUNT
+    -- as each cutsomer may have muultiple downloads per days. 
+           COUNT(CASE WHEN paying_customer = 'yes' THEN downloads END) AS paying,
+           COUNT(CASE WHEN paying_customer = 'no' THEN downloads END) AS non_paying
+    FROM ms_user_dimension a
+    INNER JOIN ms_acc_dimension b ON a.acc_id = b.acc_id
+    INNER JOIN ms_download_facts c ON a.user_id = c.user_id
+    GROUP BY c.date
+)
+SELECT 
+download_date,
+non_paying,
+paying
+FROM download_totals
+-- Wrong filter criteria.
+WHERE (non_paying - paying) > 0
+-- ASC not necessary to include it's assumed as the default.
+ORDER BY download_date ASC;
+
+---- 
+
+-- My Changes
+
+WITH download_totals AS (
+    SELECT c.date AS download_date,
+	SUM(CASE WHEN paying_customer = 'yes' THEN downloads END) AS paying,
+	SUM(CASE WHEN paying_customer = 'no' THEN downloads END) AS non_paying
+    FROM ms_user_dimension a
+    LEFT JOIN ms_download_facts c USING(user_id)
+    LEFT JOIN ms_acc_dimension b USING(acc_id)
+    GROUP BY 1
+    HAVING non_paying > paying
+)
+SELECT 
+download_date,
+non_paying,
+paying
+FROM download_totals
+ORDER BY 1
+
+
